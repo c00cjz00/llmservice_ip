9de9292b2e3033132ae44625a3d78aaefabeafaa
Ubuntu <summerhill001@gmail.com>
2024-08-21 08:06:46 +0800

    update2v1

---
+++ llmservice_two_ip_v1/01-init.sh
@@ -0,0 +1,54 @@
+# 注意: 請先 先編集 env.sample 內容
+# Example: ./01-init.sh gpu
+[[ -z "$1" ]] && { echo "Parameter 1 is empty" ; exit 1; }
+IS_GPU=$1
+
+# 目前位置
+mypwd=$(pwd)
+
+# ENV
+cp env.sample .env
+
+# DNS
+rsync -av nginx_conf/ nginx
+
+# Create Website 
+rsync -av website_conf/ website
+
+# Create Website 
+rsync -av litellm_conf/ litellm
+
+# 建立目錄
+cd ${mypwd}
+mkdir -p storage/anythingllm_data
+mkdir -p storage/anythingllm_hotdir 
+mkdir -p storage/anythingllm_outputs 
+mkdir -p storage/ollama_data 
+mkdir -p storage/openwebui_data 
+mkdir -p storage/hf_cache 
+mkdir -p storage/postgres
+mkdir -p storage/pgadmin
+mkdir -p storage/qdrant_data
+chmod 777 storage/pgadmin
+ 
+if ! [ -f ./storage/anythingllm_env.txt ]; then
+	touch ./storage/anythingllm_env.txt
+fi 
+
+if ! [ -f ./.env ]; then
+	cp ./env.sample .env
+fi
+
+
+# 複製compsoe file
+# CPU/GPU
+if [[ "$IS_GPU" == "gpu" ]] 
+then
+	cp compose/docker-compose_gpu.yml docker-compose.yml
+else
+	cp compose/docker-compose_cpu.yml docker-compose.yml
+fi
+
+# echo 
+echo PORTAL: http://\$IP
+
---
+++ llmservice_two_ip_v1/docker_clean.sh
@@ -0,0 +1,2 @@
+docker container prune -f
+docker image prune -f -a
